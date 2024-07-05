3190fe95f1d14267f896cf667646e68c02a10e9f
Yuto Kohata <ykohata@tsukuba-ws0.soirt>
2024-07-05 11:11:37 +0000

    apply affinity to ra.py

--- src/ra.py
+++ src/ra.py
@@ -1,11 +1,44 @@
 import random
 import torch
+import os
 import torchvision.transforms.functional as F
 import numpy as np
+import pandas as pd
 from torch import Tensor, nn
+from omegaconf import OmegaConf
 from typing import List, Dict, Optional, Tuple
 
 from aug_meta import DefineAugmentSpace, _apply_op
+from set_cfg import override_original_config
+
+# TODO: dataloader.py & affinity.pyにRandAugmentSegmentationクラスを元のコードを参考にして継承させる
+
+def reset_cfg(cfg, init: bool):
+    if init:
+        if cfg.augment.ra.init_epoch is None:
+            raise ValueError("Error... set num of init phase epoch")
+        print(f"Set cfg for init phase, num of init phase epoch ...{cfg.augment.ra.init_epoch}")
+
+        if cfg.augment.name[0] == "w_ra":
+            cfg.augment.ra.warmup_ra = True
+            print("Apply warmup RA")
+    
+        cfg.augment.name = ["nan"]
+        cfg.save.affinity = True
+
+    else:
+        print("Set cfg for main phase")
+        cfg.augment.name=["ra"]
+        cfg.augment.ra.weight="affinity"
+        cfg.save.affinity=False
+
+        if cfg.augment.ra.warmup_ra:
+            print("Set for warmup RA, weight is random")
+            cfg.augment.ra.weight="random"
+
+    override_original_config(cfg)
+    print(OmegaConf.to_yaml(cfg))
+    return cfg
 
 class RandAugmentSegmentation(torch.nn.module):
     def __init__(
@@ -29,7 +62,7 @@ class RandAugmentSegmentation(torch.nn.module):
         self.weight_type = cfg.augment.ra.weight
         self.softmax_t = cfg.augment.ra.softmax_t
 
-        # self.single = cfg.augment.ra.single
+        self.single = cfg.augment.ra.single
 
         self.space = DefineAugmentSpace()
         
@@ -39,6 +72,11 @@ class RandAugmentSegmentation(torch.nn.module):
             self.op_meta = self.space._jda_augmentation_space(self.num_magnitude_bins, (32, 32))
 
         self.weight = {key: 0 for key in self.op_meta.keys()}
+
+        if "ra" in self.cfg.augment.name:
+            if self.weight_type == "affinity":
+                self.metrics_value = self.get_metrics_values(self.weight)
+
         self.weight = self.get_weight(self.weight)
 
         self.iden_rate = float(1 / len(self.op_meta))
@@ -59,6 +97,12 @@ class RandAugmentSegmentation(torch.nn.module):
             elif fill is not None:
                 fill = [float(f) for f in fill]
 
+         # aug spaceの設定
+        if self.cfg.augment.ra.space == "ra":
+            op_meta = self.space._ra_augmentation_space(self.num_magnitude_bins, (height, width))
+        elif self.cfg.augment.ra.space == "jda":
+            op_meta = self.space._jda_augmentation_space(self.num_magnitude_bins, (height, width))
+
         # 指定された回数だけ拡張操作を適用
         for _ in range(self.num_ops):
             # 重みに基づいて拡張操作を選択
@@ -78,7 +122,7 @@ class RandAugmentSegmentation(torch.nn.module):
                 selected_mag *= -1.0
 
             # 重み付け方法に応じて拡張操作を適用
-            if self.weight_type == "random":
+            if self.weight_type == "affinity":
                 if random.random() < self.iden_rate:
                     op_name = list(self.op_meta.keys())[0]  # Identity
                 else:
@@ -97,22 +141,7 @@ class RandAugmentSegmentation(torch.nn.module):
 
         # 拡張された画像とラベルを返す
         return {'image': img, 'label': label}
-
-    def get_weight(self, weight):
-        # 重み付け方法に応じて重みを計算
-        if self.weight_type == "random":
-            # ランダムな場合、全ての操作に等しい重みを与える
-            weight_value = np.ones(len(weight))
-            weight_value = weight_value / sum(weight_value)
-        else:
-            raise ValueError(f"Invalid weight type: {self.weight_type}")
-
-        # 計算された重みを各操作に割り当てる
-        for i, key in enumerate(weight):
-            weight[key] = weight_value[i]
-
-        return weight
-
+    
     def save_history(self):
         pass
 
@@ -127,3 +156,54 @@ class RandAugmentSegmentation(torch.nn.module):
             f"fill={self.fill}"
             f")"
         )
+
+    def get_weight(self, weight):
+        # 重み付け方法に応じて重みを計算
+        if self.weight_type == "random":
+            # ランダムな場合、全ての操作に等しい重みを与える
+            weight_value = np.ones(len(weight))
+            weight_value = weight_value / sum(weight_value)
+
+        elif self.weight_type == "single":
+            weight[self.single] = 1.0
+            weight_value = np.array(list(weight.values()))
+
+        else:
+            weight_value = self.metrics_value
+
+            if self.cfg.augment.ra.fix_iden:
+                weight_value = np.delete(weight_value, 0)
+                weight_value = nn.functional.softmax(
+                    torch.tensor(weight_value) / self.cfg.augment.ra.softmax_t,
+                    dim=0
+                    )
+                weight_value = torch.cat([torch.tensor([0,0], dtype=torch.float64), weight_value])
+
+            else:
+                weight_value = nn.functional.softmax(
+                    torch.tensor(weight_value) / self.cfg.augment.ra.softmax_t,
+                    dim=0
+                )
+
+        # 計算された重みを各操作に割り当てる
+        for i, key in enumerate(weight):
+            weight[key] = weight_value[i]
+
+        return weight
+    
+    def get_metrics_values(self, weight):
+        file_path = (self.cfg.out_dir + "affinity.csv")
+
+        if not os.path.exists(file_path):
+            if self.cfg.augment.ra.affinity_path is None:
+                raise ValueError("affinity.csv path not found...")
+            else:
+                file_path = self.cfg.augment.ra.affinity_path
+
+        df = pd.read_csv(file_path)
+        for key in weight:
+            weight[key] = df.iloc[-1][key]
+
+        values = np.array(list(weight.values()))
+
+        return values
