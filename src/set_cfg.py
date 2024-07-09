20c4bdf7ac5c8dce8219bbeb43552b30d8f5aa87
Yuto Kohata <ykohata@tsukuba-ws0.soirt>
2024-07-09 02:55:24 +0000

    polynomialスケジューラーとsingleで制御できるロジックを追加

--- src/set_cfg.py
+++ src/set_cfg.py
@@ -24,7 +24,11 @@ def setup_config():
     else:
         config_file_name = "test"
         config_file_path = conf_dir / "test.yaml"
+<<<<<<< HEAD
     config_file_path = config_file_path = current_dir / "conf" / f"{config_file_name}.yaml"
+=======
+    config_file_path = str(current_dir) + f"/conf/{config_file_name}.yaml"
+>>>>>>> 8a19810 (polynomialスケジューラーとsingleで制御できるロジックを追加)
     if os.path.exists(config_file_path):
         cfg = OmegaConf.load(config_file_path)
     else:
